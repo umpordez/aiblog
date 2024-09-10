7e82759276020a4bc0a3782112fdd5862aebafda
Deividy Metheler Zachetti <deividyz@gmail.com>
2024-09-10 15:08:19 -0300

    add configs to work with knex

---
+++ ts/knexfile.ts
@@ -0,0 +1,18 @@
+import type { Knex } from "knex";
+
+import dotenv from "dotenv";
+import path from 'node:path';
+
+dotenv.config({ path: path.resolve('./.env') });
+
+const config: Knex.Config = {
+    client: 'pg',
+    connection: {
+        host: process.env.PSQL_HOST,
+        database: process.env.PSQL_DATABASE,
+        user: process.env.PSQL_USER,
+        password: process.env.PSQL_PASSWORD
+    }
+};
+
+export default config;
