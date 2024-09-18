c9756eee0fd65c3777031cff841ce5d7ed9ea70f
Deividy Metheler Zachetti <deividyz@gmail.com>
2024-09-18 16:47:18 -0300

    schema update tweaks

--- ts/migrations/20240910180438_first-migration.ts
+++ ts/migrations/20240910180438_first-migration.ts
@@ -3,6 +3,7 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     await knex.raw(`
         CREATE TYPE user_in_account_role AS ENUM ('admin');
+        CREATE TYPE ai_provider AS ENUM ('groq', 'openai');
 
         CREATE TABLE users (
             id uuid not null
@@ -26,6 +27,13 @@ export async function up(knex: Knex): Promise<void> {
                 primary key default(gen_random_uuid()),
 
             title text not null,
+            link text not null
+                constraint uq_accounts_link unique,
+
+            ai_provider ai_provider not null
+                constraint df_accounts_ai_provider default('groq'),
+
+            ai_api_key text,
 
             utc_created_on timestamp not null
                 constraint df_accounts_utc_created_on
@@ -42,7 +50,7 @@ export async function up(knex: Knex): Promise<void> {
                 references accounts(id),
 
             name text not null,
-            description text not null,
+            system_prompt text not null,
 
             utc_created_on timestamp not null
                 constraint df_avatars_utc_created_on
@@ -87,9 +95,11 @@ export async function down(knex: Knex): Promise<void> {
     await knex.raw(`
         DROP TABLE blog_posts;
         DROP TABLE user_in_accounts;
+        DROP TABLE avatars;
         DROP TABLE accounts;
         DROP TABLE users;
         DROP TYPE user_in_account_role;
+        DROP TYPE ai_provider;
     `);
 }
 
