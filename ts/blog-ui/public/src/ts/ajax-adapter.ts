98d0e4b930460dc81899fa0298a2df67157a88b4
Deividy Metheler Zachetti <deividyz@gmail.com>
2024-09-19 20:07:19 -0300

    add ajaxAdapter fixes eslint

---
+++ ts/blog-ui/public/src/ts/ajax-adapter.ts
@@ -0,0 +1,77 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+import config from './config.js';
+
+async function doRequest(
+    url: string,
+    method: string,
+    body: any,
+    headers: any = {}
+) {
+    headers = headers || {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    };
+
+    const errorStack = new Error();
+
+    if (method.toLowerCase() === 'get' && body) {
+        url = `${url}?${new URLSearchParams(body)}`;
+    } else if (!(body instanceof window.FormData)) {
+        body = JSON.stringify(body);
+    }
+
+    try {
+        const { baseApiUrl } = config;
+
+        const response = await fetch(`${baseApiUrl}${url}`, {
+            method: method.toUpperCase(),
+            credentials: 'include',
+            headers,
+            body
+        });
+
+        if (response.ok) {
+            if ((/json/).test(headers.Accept)) {
+                const json = await response.json();
+
+                return json;
+            }
+
+            return response;
+        }
+
+        throw response;
+    } catch (ex: any) {
+        console.error(ex);
+
+        if (ex.body) {
+            try {
+                const json = (await ex.json());
+                ex.message = json.message || json.msg;
+            } catch(err) {
+                console.error(err);
+            }
+        }
+
+        if (ex.status === 401) {
+            sessionStorage.clear();
+            localStorage.clear();
+        }
+
+        errorStack.message = ex.message || ex.statusText;
+        throw errorStack;
+    }
+}
+
+const ajaxAdapter = {
+    request: doRequest ,
+    post(url: string, body: any, headers: any) {
+        return doRequest(url, 'POST', body, headers);
+    },
+
+    get(url: string, body: any, headers: any) {
+        return doRequest(url, 'GET', body, headers);
+    }
+};
+
+export default ajaxAdapter;
--- ts/eslint.config.mjs
+++ ts/eslint.config.mjs
@@ -10,7 +10,10 @@ export default tseslint.config(
     {
         ignores: [ 'build/**/*' ],
         languageOptions: {
-            globals: { ...globals.node }
+            globals: {
+                ...globals.node,
+                ...globals.browser
+            }
         }
     }
 );
