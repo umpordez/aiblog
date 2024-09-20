6ba50207e83a5fce8d24e49e92ba37215f00ddee
Deividy Metheler Zachetti <deividyz@gmail.com>
2024-09-20 18:31:02 -0300

    add login test

--- ts/tests/models/user.test.ts
+++ ts/tests/models/user.test.ts
@@ -41,3 +41,45 @@ test('[ModelUser] can create user', async () => {
 
     userIds.push(user.id);
 });
+
+test('[ModelUser] can create login', async () => {
+    const ctx = new Context();
+    const client = new User(ctx);
+
+    const user = await client.create({
+        name: 'Foo',
+        email: 'foo',
+        password: 'bar'
+    });
+
+    assert(user);
+    assert(user.id);
+    assert(user.name === 'Foo');
+    assert(user.email === 'foo');
+
+    userIds.push(user.id);
+
+    try {
+        await client.login('foo', '123');
+        throw new Error('Unexpected');
+    } catch (ex) {
+        if (ex instanceof Error) {
+            assert(/Invalid password/.test(ex.message));
+        } else {
+            throw ex;
+        }
+    }
+
+    try {
+        await client.login('foodd', '123');
+        throw new Error('Unexpected');
+    } catch (ex) {
+        if (ex instanceof Error) {
+            assert(/User not found/.test(ex.message));
+        } else {
+            throw ex;
+        }
+    }
+
+    await client.login('foo', 'bar');
+});
