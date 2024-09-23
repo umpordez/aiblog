d5d89281f284a4b2dd8c0e3b71425c12173179e1
Deividy Metheler Zachetti <deividyz@gmail.com>
2024-09-22 21:43:05 -0300

    update dev.sh to run new services

--- ts/dev.sh
+++ ts/dev.sh
@@ -3,4 +3,7 @@
 npm run blog-ui-ts & \
 npm run blog-ui-node & \
 npm run blog-api-node & \
+npm run blog-audio & \
+npm run blog-ai-transcribe & \
+npm run blog-ai-create-post & \
 npm run blog-ui-tailwind && fg
